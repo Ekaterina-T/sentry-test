e362c33374c64b0bca9e28ea4a976101c1bd6631
ET <tarakanova_ekaterina@hotmail.com>
2026-03-24 22:39:00 +0300

    5454

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ const integrations =Sentry.getDefaultIntegrations({}).filter(
 );
 
 // const dns2 = 'https://11c23b8680c61febce852f431db4491a@o4511089344380928.ingest.de.sentry.io/4511095827791952'
-// const dns3 = 'https://8aa2640f58c8a090d64923d310ab647c@o4511089344380928.ingest.de.sentry.io/4511098540130384'
+const dns3 = 'https://8aa2640f58c8a090d64923d310ab647c@o4511089344380928.ingest.de.sentry.io/4511098540130384'
 const dns4 = 'https://6d11de15dbfe353a7da26f0f7d66e5a5@o4511089344380928.ingest.de.sentry.io/4511098687062096'
 const dns5 = 'https://8af89f614c7a0868add605b116baabbe@o4511089344380928.ingest.de.sentry.io/4511099137097808'
 
@@ -70,27 +70,27 @@ scope2.captureMessage("Test message from client 5");
 
 export {client, scope, client2, scope2};
 
-// Sentry.init({
-//   app,
-//   dsn: dns3,
-//   sendDefaultPii: false,
-//   integrations: [
-//     Sentry.browserTracingIntegration({ router }),
-//     Sentry.replayIntegration(),
-//     Sentry.consoleLoggingIntegration({ levels: ['error', 'warn'] }),
-//   ],
-//   tracesSampleRate: 1.0,
-//   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-// //   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-//   replaysSessionSampleRate: 0.1,
-//   replaysOnErrorSampleRate: 1.0,
-//   enableLogs: true,
-//   beforeSend(event) {
-//     console.log("Event captured by Sentry:", event);
-//     event.exception
-//     return event;
-//   }
-// });
+Sentry.init({
+  app,
+  dsn: dns3,
+  sendDefaultPii: false,
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration(),
+    Sentry.consoleLoggingIntegration({ levels: ['error', 'warn'] }),
+  ],
+  tracesSampleRate: 1.0,
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  enableLogs: true,
+  beforeSend(event) {
+    console.log("Event captured by Sentry:", event);
+    event.exception
+    return event;
+  }
+});
 
 // console.log("Sentry initialized:", sentryInit)
 
